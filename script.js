d54cba8017b61c205207e8ee61cfe02f59e561b9
bjulb <rzul.jan@gmail.com>
2024-03-11 21:09:03 +0100

    Style collapse button for CASE1PL

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let BugRepCase1pl_Cont = document.getElementById("case1-pl-cont");
 let BugRepCase1pl_Head = document.getElementById("case1-pl-head");
+//let BugRepHeader = document.getElementsByClassName("collapse-header")
 
 BugRepCase1pl_Cont.style.display = "none";
 BugRepCase1pl_Head.onclick = () => {
     if (BugRepCase1pl_Cont.style.display == "none") {
         BugRepCase1pl_Cont.style.display = "block";
+        BugRepCase1pl_Head.style.fontWeight = "bold";
+        BugRepCase1pl_Head.style.textAlign = "left";
     }
     else if (BugRepCase1pl_Cont.style.display != "none") {
         BugRepCase1pl_Cont.style.display = "none";
+        BugRepCase1pl_Head.style.fontWeight = "normal";
     }
 }
