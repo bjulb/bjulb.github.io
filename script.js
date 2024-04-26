b65819200a533d1e6a63184af7a4f125109e23f3
bjulb <rzul.jan@gmail.com>
2024-04-26 12:40:17 +0200

    Fixing nulls in getting html elements

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 //-----------------------OBJECTS AND CLASSES
-//Bug Report Cases Objects
+//Bug Report and Test Cases Cases Objects
+
 
 class BugCase {
     constructor(head, cont, lang) {
         this.head = head;
         this.cont = cont;
         this.lang = lang;
-        this.cont.style.display = "none";
-    }
-}
+        //this.cont.style.display = "none";
+    }};
+
+class TestCase {
+    constructor(head, cont, lang) {
+        this.head = head;
+        this.cont = cont;
+        this.lang = lang;
+        //this.cont.style.display = "none";
+    }};
 
 const BR_Case1_en = new BugCase(document.getElementById("br_case1-en-head"), document.getElementById("br_case1-en-cont"), "en");
 const BR_Case1_pl = new BugCase(document.getElementById("br_case1-pl-head"), document.getElementById("br_case1-pl-cont"), "pl");
+
+
+const TC_Case1_en = new TestCase(document.getElementById("tc_case1-en-head"), document.getElementById("tc_case1-en-cont"), "en");
+//const TC_Case1_pl = new TestCase(document.getElementById("tc_case1-pl-head"), document.getElementById("tc_case1-pl-cont"), "pl");
+
+
 //const BR_Case2_en = new BugCase(document.getElementById("br_case2-en-head"), document.getElementById("br_case2-en-cont"), "en");
 //const BR_Case2_pl = new BugCase(document.getElementById("br_case2-pl-head"), document.getElementById("br_case2-pl-cont"), "pl");
-
-
-
 
 
 //---------------------FUNCTIONS
@@ -41,13 +52,26 @@ function collapseArticle(head, cont) {
 
 //Bug Reports Collapse
 
+if (BR_Case1_en.head) {
 BR_Case1_en.head.onclick = () => {
     collapseArticle(BR_Case1_en.head, BR_Case1_en.cont);
-}
+}};
 
+if (BR_Case1_pl.head) {
 BR_Case1_pl.head.onclick = () => {
     collapseArticle(BR_Case1_pl.head, BR_Case1_pl.cont);
-}
+}};
+
+//Test Cases Collapse
+if (TC_Case1_en.head) {
+TC_Case1_en.head.onclick = () => {
+    collapseArticle(TC_Case1_en.head, TC_Case1_en.cont);
+}};
+/*
+TC_Case1_pl.head.onclick = () => {
+    collapseArticle(TC_Case1_pl.head, TC_Case1_pl.cont);
+};*/
+
 /*
 BR_Case2_en.head.onclick = () => {
     collapseArticle(BR_Case2_en.head, BR_Case2_en.cont);
@@ -57,3 +81,4 @@ BR_Case2_pl.head.onclick = () => {
     collapseArticle(BR_Case2_pl.head, BR_Case2_pl.cont);
 }
 */
+
